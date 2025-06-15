09613c7217bb339fc10669664fddc9d5b918cea8
krblackwell <43302501+krblackwell@users.noreply.github.com>
2025-06-15 09:15:22 -0400

    Deploy website - based on bc13c981fc3fab7c7b34a45fd800cc63772569e7

---
+++ assets/js/898514b1.990923e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklearn_coding_fun_projects=self.webpackChunklearn_coding_fun_projects||[]).push([[67],{9187:e=>{e.exports=JSON.parse('{"tags":[{"label":"python","permalink":"/docs/tags/python","count":5},{"label":"setup","permalink":"/docs/tags/setup","count":1}]}')}}]);
